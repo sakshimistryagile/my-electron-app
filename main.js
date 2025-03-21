bcc7708eaf5384b89de33d5e9118ff4581a111b7
sakshi mistry <sakshi.mistry@agileinfoways.com>
2025-03-21 10:40:40 +0530

    change fix

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 const { app, BrowserWindow, ipcMain } = require("electron/main");
+const { autoUpdater } = require("electron-updater");
 const path = require("node:path");
 
+let mainWindow;
 // console.log("Hello from Electron 👋");
 const createWindow = () => {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
-  win.loadURL("https://github.com");
-  const contents = win.webContents;
+  mainWindow.loadURL("https://github.com");
+  const contents = mainWindow.webContents;
   //   console.log(contents);
-  win.loadFile("index.html");
+  mainWindow.loadFile("index.html");
 };
 app.whenReady().then(() => {
   ipcMain.handle("ping", () => "pong");
@@ -26,4 +28,26 @@ app.whenReady().then(() => {
 });
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
+});
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+});
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
+
+autoUpdater.once("ready-to-show", () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
 });
